f25a95838654ab6960bd49e43bf4402622e0bc6b
Javier Carranza <javcarr.2203@gmail.com>
2025-05-25 16:10:49 -0500

    Corrigiendo connectionParams

--- config/db_config.js
+++ config/db_config.js
@@ -5,6 +5,7 @@ const connectionParams = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     connectionLimit: 20,
     queueLimit: 4
 }
